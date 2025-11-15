a8f670ded11c24efa8ab661514f740054c8af685
Piotr Walczak <pwalczak@student.agh.edu.pl>
2025-11-15 19:32:28 +0100

    Updating PBR_API for better linux support.

--- include/pbr/pbr.h
+++ include/pbr/pbr.h
@@ -1,11 +1,17 @@
 #ifndef INCLUDE_PBR_H_
 #define INCLUDE_PBR_H_
 
-#ifdef _WIN32
+#if defined(_WIN32)
   #ifdef PBR_SHARED_LIB_EXPORT
     #define PBR_API __declspec(dllexport)
   #else
     #define PBR_API __declspec(dllimport)
+  #endif
+#elif defined(__GNUC__) || defined(__clang__)
+  #ifdef PBR_SHARED_LIB_EXPORT
+    #define PBR_API __attribute__((visibility("default")))
+  #else
+    #define PBR_API
   #endif
 #else
   #define PBR_API
