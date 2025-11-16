e05834628f35daecb54763024d473fd865ca78c2
Piotr Walczak <pwalczak@student.agh.edu.pl>
2025-11-16 15:32:18 +0100

    Update gitignore and api macros

---
+++ src/pbr_api.h
@@ -0,0 +1,20 @@
+#ifndef SRC_PBRAPI_H_
+#define SRC_PBRAPI_H_
+
+#if defined(_WIN32)
+  #ifdef PBR_SHARED_LIB_EXPORT
+    #define PBR_API __declspec(dllexport)
+  #else
+    #define PBR_API __declspec(dllimport)
+  #endif
+#elif defined(__GNUC__) || defined(__clang__)
+  #ifdef PBR_SHARED_LIB_EXPORT
+    #define PBR_API __attribute__((visibility("default")))
+  #else
+    #define PBR_API
+  #endif
+#else
+  #define PBR_API
+#endif
+
+#endif
