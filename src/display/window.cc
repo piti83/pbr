14aff6f5a13a99cb3ca53ee25bfd51a90cec6b5f
Piotr Walczak <pwalczak@student.agh.edu.pl>
2025-11-24 12:54:59 +0100

    Adding logs in window creation process

--- src/display/window.cc
+++ src/display/window.cc
@@ -1,5 +1,6 @@
-#include "window.h"
-#include "exceptions/window_exception.h"
+#include <display/window.h>
+#include <exceptions/window_exception.h>
+#include <utility/logger.h>
 
 #include <iostream>
 
@@ -8,29 +9,40 @@
 
 namespace pbr {
 
+
+
 Window::Window(int width, int height, const char* title) {
   if (width <= 0) {
-    std::cout << "Invalid window width. Creating with default value (800)"
-              << std::endl;
+    Logger::Warn("Invalid window width.Creating with default value(800)");
     width_ = 800;
   } else {
     width_ = width;
   }
 
   if (height <= 0) {
-    std::cout << "Invalid window height. Creating with default value (600)"
-              << std::endl;
+    Logger::Warn("Invalid window height. Creating with default value (600)");
     height_ = 600;
   } else {
     height_ = height;
   }
 
   title_ = title;
+  window_ = nullptr;
+
+  Logger::Trace("Window object created with parameters:");
+  Logger::Trace("Width:   {}", width_);
+  Logger::Trace("Height:  {}", height_);
+  Logger::Trace("Title:   {}", title_);
 }
+
+
 
 void Window::Init() {
   if (glfwInit() != GLFW_TRUE) {
+    Logger::Error("Failed to initialise GLFW!");
     throw pbr::WindowException("Failed to initialise GLFW!");
+  } else {
+    Logger::Info("GLFW successfully initialised.");
   }
 
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -38,7 +50,10 @@ void Window::Init() {
 
   window_ = glfwCreateWindow(width_, height_, title_, nullptr, nullptr);
   if (window_ == NULL) {
+    Logger::Error("Failed to create glfw window!");
     throw pbr::WindowException("Failed to create glfw window!");
+  } else {
+    Logger::Info("Window successfully initialised.");
   }
 }
 
@@ -52,7 +67,9 @@ const char* Window::GetTitle() { return title_; }
 
 Window::~Window() {
   glfwDestroyWindow(window_);
+  Logger::Info("GLFW window destroyed.");
   glfwTerminate();
+  Logger::Info("GLFW terminated.");
 }
 
 }  // namespace pbr
